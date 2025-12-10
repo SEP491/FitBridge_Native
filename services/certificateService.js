1091b097f9557d304f1cfcf65cb4f12dc1dcc541
phonglamm <lamphong1507@gmail.com>
2025-12-11 01:40:23 +0700

    Update certificateService.js

--- services/certificateService.js
+++ services/certificateService.js
@@ -2,18 +2,17 @@ import { request } from "./request";
 
 const certificateService = {
   getCertificateForPT: (params) =>
-    request("GET", `/v1/certificates`, null, {}, params),
+    request("GET", `v1/certificates`, null, {}, params),
 
-  createCertificate: (formData) =>
-    request("POST", "/v1/certificates", formData),
+  createCertificate: (formData) => request("POST", "v1/certificates", formData),
 
-  getCertificateMetadata: () => request("GET", `/v1/certificates/metadata`),
+  getCertificateMetadata: () => request("GET", `v1/certificates/metadata`),
 
   updateCertificateStatus: (certificateId, data) =>
-    request("PUT", `/v1/certificates/${certificateId}`, data),
+    request("PUT", `v1/certificates/${certificateId}`, data),
 
   deleteCertificate: (certificateId) =>
-    request("DELETE", `/v1/certificates/${certificateId}`),
+    request("DELETE", `v1/certificates/${certificateId}`),
 };
 
 export default certificateService;
