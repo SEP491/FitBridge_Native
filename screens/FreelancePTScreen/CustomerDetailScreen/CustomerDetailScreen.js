2ed5fd046b9f9d1ca1165a64fdb5e450d60a465d
MinhLuong30 <166358474+MinhLuong30@users.noreply.github.com>
2025-12-25 10:57:01 +0700

    fix customerDetail

--- screens/FreelancePTScreen/CustomerDetailScreen/CustomerDetailScreen.js
+++ screens/FreelancePTScreen/CustomerDetailScreen/CustomerDetailScreen.js
@@ -560,8 +560,8 @@ export const CustomerDetailScreen = ({ route, navigation }) => {
                             styles.packageDetailValue,
                             packageStatus.status === t("myPackage.expired") && styles.expiredText
                           ]}>
-                            {stats?.LatestSessionStartTime ? 
-                              `${new Date(stats.LatestSessionStartTime ).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', hour12: false })} - ${new Date(stats.LatestSessionStartTime).toLocaleDateString('en-GB')}` 
+                            {stats?.latestSessionStartTime ? 
+                              `${new Date(stats.latestSessionStartTime ).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', hour12: false })} - ${new Date(stats.latestSessionStartTime).toLocaleDateString('en-GB')}` 
                               : t("customerDetail.notAvailable")
                             }
                           </Text>
